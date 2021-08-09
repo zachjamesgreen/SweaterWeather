97b5aeea7ca0846bba0c5415573dc3b3f3c7d2c8
Zach Green <zachjamesgreen@gmail.com>
2021-08-08 21:39:03 -0600

    configure VCR

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,16 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/vcr_cassettes"
+  config.hook_into :faraday
+  config.configure_rspec_metadata!
+  config.default_cassette_options = { record: :new_episodes }
+  config.filter_sensitive_data('<MAPQUEST_API_KEY>') { ENV['MAPQUEST_API_KEY'] }
+  config.filter_sensitive_data('<OPENWEATHER_API_KEY>') { ENV['OPENWEATHER_API_KEY'] }
+end
+
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
