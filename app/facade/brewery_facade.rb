f21a80d286c8ea069cb6fc83f26732047454dfec
Zach Green <zachjamesgreen@gmail.com>
2021-08-09 10:54:39 -0600

    refactor facade

--- app/facade/brewery_facade.rb
+++ app/facade/brewery_facade.rb
@@ -1,13 +1,21 @@
 class BreweryFacade
   def self.get_breweries(location, quantity)
     brew_res = BreweryService.get_breweries(location, quantity)
+    create_breweries(location)
+    add_breweries(brew_res)
+    @breweries
+  end
+
+  def self.create_breweries(location)
     lat, lng = Geocoder.geocode(location)
     weather_res = WeatherFacade.weather(lat, lng)
-    breweries = Breweries.new(weather_res[0], location)
+    @breweries = Breweries.new(weather_res[0], location)
+  end
+
+  def self.add_breweries(brew_res)
     json = JSON.parse(brew_res.body)
     json.each do |brewery|
-      breweries.breweries << Brewery.new(brewery)
+      @breweries.breweries << Brewery.new(brewery)
     end
-    breweries
   end
 end
