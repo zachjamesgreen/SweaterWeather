9fc50f843f9f8921609347ca7f8bc41dd94d730e
Zach Green <zachjamesgreen@gmail.com>
2021-08-10 13:47:55 -0600

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: {:case_sensitive => false}
+
   has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { should have_secure_password }
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email).case_insensitive }
+  it { should validate_presence_of(:password) }
 end
